dd6d6addbcf8d486b17a1ac8dc5a8be6afe15cd1
James Felts <jfelts1@users.noreply.github.com>
2015-07-09 22:58:04 -0700

    fixed crash in menu options 5 and 6 added equality check options in menu

--- assign1/assign1/assign1/complex_tester.cpp
+++ assign1/assign1/assign1/complex_tester.cpp
@@ -1,39 +1,11 @@
 #include "complex.h"
+#include <limits>
 
-bool menu(Complex c1, Complex c2);
+bool menu(Complex& c1, Complex& c2);
 void getComplexValueFromUser(Complex& complex);
 
 int main()
 {
-	/*Complex c1(0, -1), c2(0, -1), c3, c4(5,2), c5(1,-5), c6(0,-2), c7(1,1),c8(-2,0), c9;
-
-	cout <<"c1: "<< c1 << "\nc2: " << c2 << endl;
-
-	c3 = c1*c2;
-	cout << "c3: " << c3 << endl;
-	cout << "c4: " << c4 << "\nc5: " << c5 << "\nc6: " << c6 << "\nc7: " << c7 << "\nc8: " << c8 << "\nc9: " << c9 << endl;
-	Complex ci(5, 5);
-	cout << "ci: " << ci << endl;
-	cin >> ci;
-	cout << "ci: " << ci << endl;
-	if (ci == c4)
-	{
-		cout << "ci: " << ci << " is equal to c4: " << c4 << endl;
-	}
-	else
-	{
-		cout << "ci: " << ci << " is not equal to c4: " << c4 << endl;
-	}
-
-	if (ci != c5)
-	{
-		cout << "ci: " << ci << " is not equal to c5: " << c5 << endl;
-	}
-	else
-	{
-		cout << "ci: " << ci << " is equal to c5: " << c5 << endl;
-	}*/
-
 	Complex c1, c2;
 	getComplexValueFromUser(c1);
 	getComplexValueFromUser(c2);
@@ -43,27 +15,33 @@ int main()
 	return 0;
 }
 
-bool menu(Complex c1, Complex c2)
+bool menu(Complex& c1, Complex& c2)
 {
 	int choice = 0;
 	Complex temp;
 	string menuText = 
-	"Complex Number Menu\n\
+	"\nComplex Number Menu\n\
 1) Quit\n\
 2) Add\n\
 3) Subtract\n\
 4) Multiply\n\
 5) New c1\n\
-6) New c2\n";
+6) New c2\n\
+7) Equal\n\
+8) Not Equal\n";
 
 	cout << menuText << endl;
+	cout << "c1 = " << c1 << endl;
+	cout << "c2 = " << c2 << endl<<endl;
 
 	cin >> choice;
-	while (choice > 6 || choice < 1)
+	while (choice > 8 || choice < 1)
 	{
 		cout << "Invalid Input\n" << endl; 
 		cin >> choice;
 	}
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	switch (choice)
 	{
 		case 1:
@@ -86,6 +64,26 @@ bool menu(Complex c1, Complex c2)
 			break;
 		case 6:
 			getComplexValueFromUser(c2);
+			break;
+		case 7:
+			if (c1 == c2)
+			{
+				cout << "true" << endl;
+			}
+			else
+			{
+				cout << "false" << endl;
+			}
+			break;
+		case 8:
+			if (c1 != c2)
+			{
+				cout << "true" << endl;
+			}
+			else
+			{
+				cout << "false" << endl;
+			}
 			break;
 		default:
 			break;
