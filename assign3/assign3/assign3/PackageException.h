fd787f2bcaba706dee7936472f772b8b0648d3e8
James Felts <jfelts1@users.noreply.github.com>
2015-07-21 04:24:11 -0700

    fixed exception issue on linux

--- assign3/assign3/assign3/PackageException.h
+++ assign3/assign3/assign3/PackageException.h
@@ -4,12 +4,16 @@
 #include "Package.h"
 #include <exception>
 #include <stdexcept>
+#include <string>
 class PackageException :
 	public Package, public std::exception
 {
 public:
-	PackageException(int trackingNumber, double weight, double shippingCost,const char* message) :Package(trackingNumber, weight, shippingCost),exception(message){};
+	PackageException(int trackingNumber, double weight, double shippingCost, const char* message) :Package(trackingNumber, weight, shippingCost), exception() { _message = message; };
 	virtual ~PackageException() {};
+	virtual const char* what()const override { return _message; }
+private:
+	const char* _message;
 };
 
 #endif
